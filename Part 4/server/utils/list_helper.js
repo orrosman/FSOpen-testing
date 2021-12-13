9ad74e66802887566a674b834a7104ca97842330
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-13 15:23:43 +0200

    4.4 - counting likes + test

--- Part 4/server/utils/list_helper.js
+++ Part 4/server/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = (blogs) => {
+	return blogs.reduce((sum, { likes }) => sum + likes, 0);
+};
+
 module.exports = {
 	dummy,
+	totalLikes,
 };
