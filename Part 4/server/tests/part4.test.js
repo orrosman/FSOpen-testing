b2362ef497e29dc25215f0848f41b01cda95c74e
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-14 17:11:13 +0200

    moved all mocks data to "mocks" dir

--- Part 4/server/tests/part4.test.js
+++ Part 4/server/tests/part4.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Blog = require('../models/Blog');
+const BlogMocks = require('./mocks/blogs');
 const supertest = require('supertest');
 const app = require('../index');
 
@@ -7,84 +8,9 @@ const request = supertest(app);
 
 const listHelper = require('../utils/list_helper');
 
-const mockBlogs = [
-	{
-		_id: '5a422a851b54a676234d17f7',
-		title: 'React patterns',
-		author: 'Michael Chan',
-		url: 'https://reactpatterns.com/',
-		likes: 7,
-		__v: 0,
-	},
-	{
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-		__v: 0,
-	},
-	{
-		_id: '5a422b3a1b54a676234d17f9',
-		title: 'Canonical string reduction',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-		likes: 12,
-		__v: 0,
-	},
-	{
-		_id: '5a422b891b54a676234d17fa',
-		title: 'First class tests',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-		likes: 10,
-		__v: 0,
-	},
-	{
-		_id: '5a422ba71b54a676234d17fb',
-		title: 'TDD harms architecture',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-		likes: 0,
-		__v: 0,
-	},
-	{
-		_id: '5a422bc61b54a676234d17fc',
-		title: 'Type wars',
-		author: 'Robert C. Martin',
-		url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-		likes: 2,
-		__v: 0,
-	},
-];
-
-const fakePostBlog = {
-	title: 'Testing Post',
-	author: 'Test Testington',
-	url: 'https://testtest.example/',
-	likes: 42,
-};
-
-const fakePostBlogNoLike = {
-	title: 'Testing Post',
-	author: 'Test Testington',
-	url: 'https://testtest.example/',
-};
-
-const fakePostBlogNoTitle = {
-	author: 'Test Testington',
-	likes: 42,
-};
-
-const fakeUpdatedBlog = {
-	title: 'React New patterns',
-	author: 'Michael Test Chan',
-	likes: 222,
-};
-
 beforeEach(async () => {
 	await Blog.deleteMany({});
-	await Blog.insertMany(mockBlogs);
+	await Blog.insertMany(BlogMocks.mockBlogs);
 });
 
 describe('Basic blog routers tests:', () => {
@@ -96,17 +22,17 @@ describe('Basic blog routers tests:', () => {
 	});
 
 	test('Sum all blog posts likes', () => {
-		const totalLikes = listHelper.totalLikes(mockBlogs);
+		const totalLikes = listHelper.totalLikes(BlogMocks.mockBlogs);
 		expect(totalLikes).toBe(36);
 	});
 
 	test('Blog post with most likes', () => {
-		const mostLikedPost = listHelper.favoriteBlog(mockBlogs);
-		expect(mostLikedPost).toEqual(mockBlogs[2]);
+		const mostLikedPost = listHelper.favoriteBlog(BlogMocks.mockBlogs);
+		expect(mostLikedPost).toEqual(BlogMocks.mockBlogs[2]);
 	});
 
 	test('Author with most blog posts', () => {
-		const highestQuantityAuthor = listHelper.mostBlogs(mockBlogs);
+		const highestQuantityAuthor = listHelper.mostBlogs(BlogMocks.mockBlogs);
 		expect(highestQuantityAuthor).toEqual({
 			author: 'Robert C. Martin',
 			blogs: 3,
@@ -114,7 +40,9 @@ describe('Basic blog routers tests:', () => {
 	});
 
 	test('Most liked author', () => {
-		const highestQuantityAuthor = listHelper.mostLikedAuthor(mockBlogs);
+		const highestQuantityAuthor = listHelper.mostLikedAuthor(
+			BlogMocks.mockBlogs
+		);
 		expect(highestQuantityAuthor).toEqual({
 			author: 'Edsger W. Dijkstra',
 			likes: 17,
@@ -125,7 +53,7 @@ describe('Basic blog routers tests:', () => {
 describe('test server - part 4 section B', () => {
 	test('Blogs are returned as json', async () => {
 		const response = await request.get('/api/blogs');
-		expect(response.body.length).toBe(mockBlogs.length);
+		expect(response.body.length).toBe(BlogMocks.mockBlogs.length);
 	});
 
 	test('Test ID exists', async () => {
@@ -136,39 +64,44 @@ describe('test server - part 4 section B', () => {
 	});
 
 	test('Create new blog', async () => {
-		const response = await request.post('/api/blogs').send({ ...fakePostBlog });
-		expect(response.body).toEqual(expect.objectContaining(fakePostBlog));
+		const response = await request
+			.post('/api/blogs')
+			.send({ ...BlogMocks.fakePostBlog });
+		expect(response.body).toEqual(
+			expect.objectContaining(BlogMocks.fakePostBlog)
+		);
 	});
 
 	test('New blog post without like property would default to 0', async () => {
 		const response = await request
 			.post('/api/blogs')
-			.send({ ...fakePostBlogNoLike });
+			.send({ ...BlogMocks.fakePostBlogNoLike });
 		expect(response.body).toEqual(
-			expect.objectContaining({ ...fakePostBlogNoLike, likes: 0 })
+			expect.objectContaining({ ...BlogMocks.fakePostBlogNoLike, likes: 0 })
 		);
 	});
 
 	test('New blog post without a title & url properties would return 400', async () => {
 		const response = await request
 			.post('/api/blogs')
-			.send({ ...fakePostBlogNoTitle });
+			.send({ ...BlogMocks.fakePostBlogNoTitle });
 		expect(response.statusCode).toBe(400);
 	});
 
 	test('Can delete a post', async () => {
 		const response = await request
 			.delete('/api/blogs')
-			.send({ id: mockBlogs[0]._id });
-		expect(response.body).toEqual(mockBlogs[0]);
+			.send({ id: BlogMocks.mockBlogs[0]._id });
+		expect(response.body).toEqual(BlogMocks.mockBlogs[0]);
 	});
 
 	test('Can update a blog post', async () => {
-		const response = await request
-			.patch('/api/blogs')
-			.send({ id: mockBlogs[0]._id, updates: { ...fakeUpdatedBlog } });
+		const response = await request.patch('/api/blogs').send({
+			id: BlogMocks.mockBlogs[0]._id,
+			updates: { ...BlogMocks.fakeUpdatedBlog },
+		});
 
-		expect(response.body).toEqual(mockBlogs[0]);
+		expect(response.body).toEqual(BlogMocks.mockBlogs[0]);
 	});
 
 	afterAll(() => {
