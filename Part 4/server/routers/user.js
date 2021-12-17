46da6292dacbc2c11504b3a530ffb2015ba8b5a8
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-17 18:26:25 +0200

    added a key to token data & the name of the user

--- Part 4/server/routers/user.js
+++ Part 4/server/routers/user.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
 				id: user._id,
 				name: user.name,
 			});
-			res.json(token);
+			res.json({ token: token });
 		} else {
 			res.json('Password in invalid');
 		}
